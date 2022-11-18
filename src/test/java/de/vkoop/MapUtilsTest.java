335f88597233d0aadaa74ed0e0866dd2dcf6658f
Viktor Koop <viktor.koop@googlemail.com>
2022-11-18 20:38:39 +0100

    cleanup imports

--- src/test/java/de/vkoop/MapUtilsTest.java
+++ src/test/java/de/vkoop/MapUtilsTest.java
@@ -1,11 +1,12 @@
 package de.vkoop;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MapUtilsTest {
 
